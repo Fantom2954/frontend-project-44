5e505744c09f66d3e597894fa0be3fcd2fd95927
Anton <minirobofango@gmail.com>
2024-02-06 12:02:56 +0700

    ActionKasha

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@
 import 'readline-sync';
 
 export default function greetUser() {
-    console.log(brain-games)
+    // console.log(brain-games)
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
